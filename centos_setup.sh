d0b3dc807e069dfd8ec9d1e69f3b156f88fe0d78
bryan2u <56008129+bryan2u@users.noreply.github.com>
2019-10-01 04:35:06 +0300

    Modified some wording

--- centos_setup.sh
+++ centos_setup.sh
@@ -7,12 +7,11 @@ setenforce 0
 sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 echo "Disabled selinux"
 hostnamectl set-hostname openstack-vm
-sudo -i
-echo "Set hostname" 
+echo "Set 'openstack-vm' hostname" 
 yum -y update
 yum -y install centos-release-openstack-rocky
 yum -y update
 yum -y install openstack-packstack
-echo "Update packages and install openstack rocky repository and packstack"
+echo "Updated packages and install openstack rocky repository and packstack"
 echo 'Manual steps: Setup your static IP address and fdisk /dev/sdb ' 
 echo 'Once done, you can run "packstack --answer-file answers.txt" ' 
